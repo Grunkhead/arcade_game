5830f41c030e4012a1bafe5012fa7597e9797313
Grunkhead <dave_windsurfen@hotmail.com>
2019-05-17 02:03:32 +0200

    Some more refactoring.

--- src/objects/unicorn.ts
+++ src/objects/unicorn.ts
@@ -24,6 +24,10 @@ export class Unicorn extends Phaser.Physics.Arcade.Sprite {
     private keyUp:    number = 87;
     private keyDown:  number = 83;
 
+    keys: Object<number> = {
+
+    }
+
     constructor(scene: NormalScene, x: number, y: number) {
         super(scene, x, y, 'unicornOne')
 
@@ -33,16 +37,6 @@ export class Unicorn extends Phaser.Physics.Arcade.Sprite {
         this.x = x;
         this.y = y;
 
-        this.cursors = this.scene.input.keyboard.createCursorKeys()
-        console.log("the cursors work!")
-        
-        this.scene.add.existing(this)
-        this.scene.physics.add.existing(this)
-
-        this.setCollideWorldBounds(true)
-        this.setBounce(0.1)
-        this.setDragX(800)
-
         this.setEventListeners();
         this.setPhysics();
         
@@ -50,62 +44,43 @@ export class Unicorn extends Phaser.Physics.Arcade.Sprite {
     }
 
     private setPhysics(): void {
+        this.scene.physics.add.existing(this);
+        this.body.setAllowGravity(true);
+        this.setCollideWorldBounds(true)
+        
         // Add some extra width and height because of smaller hitbox.
         this.setSize(this.displayWidth + 60, this.displayHeight + 60);
-
-        this.body.setAllowGravity(true);
-        this.scene.physics.add.existing(this);
     }
 
     // Update the game based on logic or input.
     public update(): void {
-
-            // Move left an right 
-        if (this.cursors.left.isDown) {
-            this.setVelocityX(-200)
-            // this.flipX = true
-        } else if (this.cursors.right.isDown) {
-            this.setVelocityX(200)
-            // this.flipX = false
-        } 
-
-        // Move up and down.
-        if (this.cursors.up.isDown) {
-            this.setVelocityY(-350)
-        } else if (this.cursors.down.isDown) {
-            this.setVelocityY(350)
-        }
-        
-        // Jump when the body is touching the floor.
-        let grounded = this.body.touching.down 
-        if (this.cursors.up.isDown && grounded) {
-            this.setVelocityY(-400)
-        }
-
         this.move();
     }
 
+    // Bind the 
     private move(): void {
         this.x += this.speed_x;
         this.y += this.speed_y;
     }
 
+    // Increase speed when a specific key is pressed.
     private onKeyDown(e: KeyboardEvent): void {
-        if (e.keyCode == 65) { this.speed_x -= 5; }
-        if (e.keyCode == 68) { this.speed_x += 5; }
-        if (e.keyCode == 87) { this.speed_y -= 5; }
-        if (e.keyCode == 83) { this.speed_y += 5; }
+        if (e.keyCode == this.keyLeft)  { this.speed_x -= 5; }
+        if (e.keyCode == this.keyRight) { this.speed_x += 5; }
+        if (e.keyCode == this.keyUp)    { this.speed_y -= 5; }
+        if (e.keyCode == this.keyDown)  { this.speed_y += 5; }
     }
 
+    // Reset a specific speed when a key is released.
     private onKeyUp(e: KeyboardEvent): void {
-        if (e.keyCode == 65) { this.speed_x = 0; }
-        if (e.keyCode == 68) { this.speed_x = 0; }
-        if (e.keyCode == 87) { this.speed_y = 0; }
-        if (e.keyCode == 83) { this.speed_y = 0; }
+        if (e.keyCode == this.keyLeft)  { this.speed_x = 0; }
+        if (e.keyCode == this.keyRight) { this.speed_x = 0; }
+        if (e.keyCode == this.keyUp)    { this.speed_y = 0; }
+        if (e.keyCode == this.keyDown)  { this.speed_y = 0; }
     }
 
     setEventListeners(): void {
-        // These listeners listen to keyboard events.
+        // Listen to the key up and down events.
         document.addEventListener("keydown", (e: KeyboardEvent) => this.onKeyDown(e));
         document.addEventListener("keyup", (e: KeyboardEvent) => this.onKeyUp(e));
     }
